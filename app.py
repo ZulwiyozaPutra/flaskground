3d7a078bb887da91c4e2b4e44f7d07f0ad3aa97b
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-11-09 12:50:48 +0700

    basic app

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, jsonify, request, render_template
+from stores import stores
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/stores/", methods=["POST"])
+def post_store():
+    data = request.get_json()
+    store = {
+        "name": data["name"],
+        "items": []
+    }
+    stores.append(store)
+    message = "a store %s added!" % store
+    return jsonify({"message": message})
+
+
+@app.route("/stores/<string:name>/")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return jsonify(store)
+        else:
+            message = "There is no store named %s" % name
+            return jsonify({"message": message})
+
+
+@app.route("/stores/")
+def get_stores():
+    json_stores = jsonify({"stores": stores})
+    return json_stores
+
+
+@app.route("/stores/<string:name>/item/", methods=["POST"])
+def post_item(name):
+    for store in stores:
+        if store["name"] == name:
+            data = request.get_json()
+            item = {
+                "name": data["name"],
+                "price": data["price"]
+            }
+            stores["name"]["items"].append(item)
+            message = "an item %s added!" % item
+            return jsonify({"message": message})
+        else:
+            message = "There is no store named %s" % name
+            return jsonify({"message": message})
+
+
+@app.route("/stores/<string:name>/item/")
+def get_item(name):
+    for store in stores:
+        if store["name"] == name:
+            return jsonify(store["items"])
+        else:
+            message = "There is no store named %s" % name
+            return jsonify({"message": message})
+
+
+app.run(debug=True)
---
+++ stores.py
@@ -0,0 +1,41 @@
+stores = [
+    {
+        "name": "Walmart",
+        "items": [
+            {
+                "name": "Soap",
+                "price": 6.99,
+            },
+            {
+                "name": "Brush",
+                "price": 1.99,
+            },
+        ]
+    },
+    {
+        "name": "Walgreens",
+        "items": [
+            {
+                "name": "Soap",
+                "price": 5.99,
+            },
+            {
+                "name": "Brush",
+                "price": 2.99,
+            },
+        ]
+    },
+    {
+        "name": "CVS",
+        "items": [
+            {
+                "name": "Soap",
+                "price": 6.99,
+            },
+            {
+                "name": "Brush",
+                "price": 0.99,
+            },
+        ]
+    },
+]
